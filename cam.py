7d02b89152939df1884395ae1bab18a4b40df49b
edbenitez <ebenit6@uic.edu>
2020-07-26 19:35:29 -0500

    add second thread for live view

--- cam.py
+++ cam.py
@@ -4,23 +4,18 @@ import threading
 from log_setup import logger
 
 class Camera():
-	def __init__(self, window_name='cvard'):
-		self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
+	def __init__(self, window_name='test'):
+		self.cap = cv2.VideoCapture(self.gstreamer_pipeline())
+		#self.cap = cv2.VideoCapture(self._gst_str())
 		self.window_name = window_name
-		cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
-		cv2.resizeWindow(640,480)
-		cv2.moveWindow(self.window_name, 0, 0)
-		cv2.setWindowTitle(self.window_name, 'Live View')
-		font = cv2.FONT_HERSHEY_PLAIN
 		
-
-	def _gst_str(self):
-		return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+	def gstreamer_pipeline(self):
+		return ("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink")
 
 	def begin(self):
 		logger.info('Start camera. Open Video Capture object')
 		if not self.cap.isOpened():
-			self.cap.open(self._gst_str(), cv2.CAP_GSTREAMER)
+			self.cap.open(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 		
 		logger.info('Starting thread for video capture')
 		if not hasattr(self, 'thread') or not self.thread.isAlive():
@@ -35,17 +30,56 @@ class Camera():
 		logger.info('Joining video capture thread')
 		if hasattr(self, 'thread'):
 			self.thread.join()
+		
+		if hasattr(self, 'thread_2'):
+			logger.info('Joining live view thread')
+			self.thread_2.join()
+
+		logger.info('Closing window(s) and de-alloc any assoc. memory')
+		cv2.destroyAllWindows()	
 
 	def view(self):
-		logger.info('Starting live view')
+		if not hasattr(self, 'thread_2') or not self.thread_2.isAlive():
+			logger.info('Starting live view')
+      # Create window and configure it
+			cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+			cv2.resizeWindow(self.window_name, 640, 480)
+			cv2.moveWindow(self.window_name, 0, 0)
+			cv2.setWindowTitle(self.window_name, 'Live View')
+			font = cv2.FONT_HERSHEY_PLAIN
+					
+			self.thread_2 = threading.Thread(target=self._view)
+			self.thread_2.start()
+
+	def _view(self):
 		while cv2.getWindowProperty(self.window_name, 0) >= 0:
 			cv2.imshow(self.window_name, self.image)
+	
+			# if user hits 'q' key, close window
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+			# if capture is off
+			if not self.ret:
+				break
 
 	def _capture(self):
 		while True:
+			# read frame and allow in-place modification
 			ret, frame = self.cap.read()
-			print(type(frame))
+			self.ret = ret
 			if ret:
 				self.image = frame
+				self.image.setflags(write=1) 
 			else:
 				break
+
+if __name__ == "__main__":
+	logger.info('Running quick camera test')
+	try:
+		cam = Camera()
+		cam.begin()
+		cam.view()
+	except:
+		cam.end()
+
+
