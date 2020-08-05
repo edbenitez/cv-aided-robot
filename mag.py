dff65d0950b0d4875d376768d3612282f8a10a09
edbenitez <ebenit6@uic.edu>
2020-08-04 23:04:52 -0500

    added calibration

--- mag.py
+++ mag.py
@@ -2,8 +2,9 @@ import time
 import board
 import busio
 import adafruit_lsm303dlh_mag
-from math import degrees, atan2
+from math import degrees, atan2, pi
 from log_setup import logger
+from numpy import interp
 
 class Mag():
 	def __init__(self):
@@ -14,18 +15,51 @@ class Mag():
 	def read_compass(self):
 		return self.sensor.magnetic
 		
-	def _vector_to_deg(self, x, y):
-		angle = degrees(atan2(y,x))
-		if angle < 0:
-			angle += 360
-		return angle
-
 	def heading(self):
-		mag_x, mag_y, _ = self.sensor.magnetic
-		return self._vector_to_deg(mag_x, mag_y)
+		mag_x, mag_y, mag_z = self.sensor.magnetic
+		mag_x = interp(mag_x, [35, 80], [-2*pi, 2*pi])
+		mag_y = interp(mag_y, [-60, -16], [-2*pi, 2*pi])
+		heading = degrees(atan2(mag_y, mag_x))
+		
+		#Formula: (deg + (min / 60.0)) / (180 / M_PI)
+		#declin_angle = (3 + (50.0/60.0)) / (180.0 / pi)
+		#heading += declin_angle
+		
+		if heading < 0:
+			heading += 360
 
+		print(heading)	
+		return (heading)
+
+	#----------------------------------------------------------
+	#	Run calibration to collect raw min and max and their range
+	#	for each x and y. Then use 2 point calibration to map the
+	# raw values to the expected range. See heading() above.
+	def calibrate(self):
+		min_x = float("inf")
+		min_y = float("inf")
+		max_x = float("-inf")
+		max_y = float("-inf")
+		
+		for i in range(1000):
+			mag_x, mag_y, _ = self.sensor.magnetic
+			min_x = min(min_x, mag_x)
+			min_y = min(min_y, mag_y)
+			max_x = max(max_x, mag_x)
+			max_y = max(max_y, mag_y)
+		
+			offset_x = (min_x + min_y) / 2.0
+			offset_y = (min_y + min_y) / 2.0
+			print(offset_x, offset_y)
+			time.sleep(0.1)		
+		print('Min x: ',min_x)
+		print('Min y: ',min_y)
+		print('Max x: ',max_x)
+		print('Max y: ',max_y)
+		return offset_x, offset_y
+ 
 if __name__ == "__main__":
 	compass = Mag()
+	#compass.calibrate()
 	while True:
-		print('Reading heading: ',compass.heading())
-
+		compass.heading()
